b252fb2f2a0b8ec8eecdb017ee0767e3703741bc
Marharyta Amosava <mozheliukrita@gmail.com>
2024-12-02 19:17:54 +0100

    TMS Lesson 16
    Added equals and hashCode methods to compare students by name to avoid duplicates in a set

--- src/com/teachmeskills/hw/lesson_16/model/Student.java
+++ src/com/teachmeskills/hw/lesson_16/model/Student.java
@@ -1,5 +1,7 @@
 package com.teachmeskills.hw.lesson_16.model;
 
+import java.util.Objects;
+
 public class Student {
 
     String name;
@@ -11,5 +13,18 @@ public class Student {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
